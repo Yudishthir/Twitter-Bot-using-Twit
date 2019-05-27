5234410c18a8859e0ad657ed8a96659fd6ca5577
Yudishthir Lokhande <yudishthir11@gmail.com>
2019-05-27 10:58:57 +0530

    Removed keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         'CduLhijBep9MqIHxeiTfEiC67',
-  consumer_secret:      'IQRwHFRYQ5lIsRAX63W4o9cnowsqlNM0AoXmXLmAKLOCyBWnYr',
-  access_token:         '1132209192037601280-fnEFubskhQeH6bQSc5Ao3kE2eY5pML',
-  access_token_secret:  'SNbZcvU6CkK1Cjjm6guCiPuTdMENRAQpBfY2PjXyKNvap',
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
